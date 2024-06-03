cc4cb2a9a8b5f5817ebe656eac01b8e031c62fcc
Michael <you@example.com>
2024-06-03 00:10:36 -0400

    some stuff working

--- src/python/findmy-plotlocationhistory.py
+++ src/python/findmy-plotlocationhistory.py
@@ -4,6 +4,8 @@ import folium
 import threading
 from time import sleep
 
+from datetime import datetime
+
 with open('location_history.json') as f:
 	location_history = json.load(f)
 
@@ -30,7 +32,8 @@ color_mapping = {dates[i]: colors[i] for i in range(len(dates))}
 
 for j in location_history:
     location = [j['lat'], j['lon']]
-    time = j["time"].rsplit(":", 2)[0].replace("T", " ")
+    # time = j["time"].rsplit(":", 2)[0].replace("T", " ")
+    time = datetime.strptime(j['time'], "%Y-%m-%dT%H:%M:%S%z").strftime("%Y-%m-%d %H:%M:%S")
     marker_color = color_mapping[j['time'].split('T')[0]]
     folium.Marker(
         location,
@@ -54,16 +57,65 @@ if 'DEVICE_NAME' in os.environ:
     DEVICE_NAME = os.environ['DEVICE_NAME']
 print(f'Showing location plot for \'{DEVICE_NAME}\'')
 
-html_range_slider = '''
+first_datetime = datetime.strptime(location_history[0]['time'], "%Y-%m-%dT%H:%M:%S%z")
+last_datetime = datetime.strptime(location_history[-1]['time'], "%Y-%m-%dT%H:%M:%S%z")
+first_time_string = first_datetime.strftime("%Y-%m-%d %H:%M:%S")
+last_time_string = last_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+html_range_slider = f'''
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script>
     <script src="https://cdn.cdnhub.io/jQRangeSlider/5.8.3/jQAllRangeSliders-min.js"></script>
     <link rel="stylesheet" id="themeCSS" href="https://cdn.cdnhub.io/jQRangeSlider/5.7.2/css/classic.min.css" type="text/css" />
 
-    <div id="slider" style="position:absolute;bottom:00%;width:100%;text-align:center;z-index:1000000"></div>
+    <div id="slider" style="position:absolute;bottom:0%;width:70%;margin-left:15%;z-index:1000000"></div>
     <script>
-        $(document).ready(function() {
-            $("#slider").dateRangeSlider();
-        });
+        $(document).ready(function() {{
+            $("#slider").dateRangeSlider({{
+                formatter: function(val){{
+                    var minutes = val.getMinutes(),
+                    hours = val.getHours(),
+                    days = val.getDate(),
+                    month = val.getMonth() + 1,
+                    year = val.getFullYear();
+                    return year + "-" + month + "-" + days + " @ " + String("0" + hours).slice(-2) + ":" + String("0" + minutes).slice(-2);
+                }},
+                bounds: {{
+                    min: new Date({first_datetime.year}, {first_datetime.month - 1}, {first_datetime.day}, {first_datetime.hour}, 0, 0),
+                    max: new Date({last_datetime.year}, {last_datetime.month - 1}, {last_datetime.day}, {last_datetime.hour + 1}, 0, 0)
+                }},
+                defaultValues: {{
+                    min: new Date({first_datetime.year}, {first_datetime.month - 1}, {first_datetime.day}, {first_datetime.hour}, 0, 0),
+                    max: new Date({last_datetime.year}, {last_datetime.month - 1}, {last_datetime.day}, {last_datetime.hour + 1}, 0, 0)
+                }},
+                step: {{
+                    minutes: 5
+                }}
+            }});
+            $("#slider").bind("valuesChanged", function(e, data){{
+                console.log("Values just changed. min: " + data.values.min + " max: " + data.values.max);
+                markers = $(".leaflet-marker-pane").children()
+                shadows = $(".leaflet-shadow-pane").children()
+                for (let i=0;i<markers.length;i++) {{
+                    markers[i].click();
+                    let tagText = $(".leaflet-popup-pane").children()[0].children[0].children[0].children[0].textContent
+                    const tagTextArr = tagText.split(" ")
+                    let date = tagTextArr[1]
+                    let time = tagTextArr[2]
+                    let datetime = date + " " + time
+                    console.log(datetime)
+                    let markerDate = new Date(datetime)
+                    console.log(markerdate)
+                    console.log(tagText)
+                    if (markerDate > data.values.min && markerDate < data.values.max) {{
+                        markers[i].style.display = 'none'
+                        shadows[i].style.display = 'none'
+                    }} else {{
+                        markers[i].style.display = 'inline'
+                        shadows[i].style.display = 'inline'
+                    }}
+                }}
+            }});
+        }});
     </script>
 '''
 map.get_root().html.add_child(folium.Element(html_range_slider))
@@ -71,11 +123,12 @@ map.get_root().html.add_child(folium.Element(html_range_slider))
 html_heading = f'''
     <div style="position:fixed;top:0%;width:100%;text-align:center;z-index:1000000">
         <h1>{DEVICE_NAME}</h1>
-        <h4>First observation: {location_history[0]['time']}</h4>
-        <h4>Last observation: {location_history[-1]['time']}</h4>
+        <h4>First observation: {first_time_string}</h4>
+        <h4>Last observation: {last_time_string}</h4>
         <div id="slider"></div>
     </div>
 '''
+
 map.get_root().html.add_child(folium.Element(html_heading))
 
 map.show_in_browser()
